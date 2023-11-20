7633d4f7c0407e0b688d86201fcb88019cf9e433
= <rishabhtiwari26>
2023-11-20 19:13:24 +0530

    response request

--- server.js
+++ server.js
@@ -2,7 +2,28 @@ const http = require('http')
 let count=0
 let server = http.createServer((req,res)=>{
     count++
-    console.log('rishabh',count,req)
-    
+
+    if (req.url=='/home'){
+        res.write('<html>')
+        res.write('<head><title>My First Page</title></head>')
+        res.write('<body><h2>Welcome home</h2></body>')
+        res.write('</html>')
+        res.end()
+    }
+    else if (req.url=='/about'){
+        res.write('<html>')
+        res.write('<head><title>My First Page</title></head>')
+        res.write('<body><h2>Welcome to About us page</h2></body>')
+        res.write('</html>')
+        res.end()
+    }
+    else if (req.url=='/node'){
+        res.write('<html>')
+        res.write('<head><title>My First Page</title></head>')
+        res.write('<body><h2>Welcome to Node project page</h2></body>')
+        res.write('</html>')
+        res.end()
+    }
+
 })
 server.listen(4000)
