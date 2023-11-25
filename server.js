4c114d4dfc0f9c29a58f0211824db632b86548fa
= <rishabhtiwari26>
2023-11-25 21:19:43 +0530

    chatApp with server

--- server.js
+++ server.js
@@ -1,29 +1,58 @@
 const http = require('http')
+const fs = require('fs')
 let count=0
 let server = http.createServer((req,res)=>{
-    count++
+    
 
-    if (req.url=='/home'){
+    if (req.url=='/'){
         res.write('<html>')
         res.write('<head><title>My First Page</title></head>')
-        res.write('<body><h2>Welcome home</h2></body>')
+        res.write("<body><h2>Welcome home</h2><form action='/message' method='POST'><input type='text' name='msg'><button type='submit'>Send</button></form></body>")
         res.write('</html>')
-        res.end()
+        return res.end()
     }
-    else if (req.url=='/about'){
-        res.write('<html>')
-        res.write('<head><title>My First Page</title></head>')
-        res.write('<body><h2>Welcome to About us page</h2></body>')
-        res.write('</html>')
-        res.end()
-    }
-    else if (req.url=='/node'){
-        res.write('<html>')
-        res.write('<head><title>My First Page</title></head>')
-        res.write('<body><h2>Welcome to Node project page</h2></body>')
-        res.write('</html>')
-        res.end()
-    }
+    // else if (req.url=='/about'){
+    //     res.write('<html>')
+    //     res.write('<head><title>My First Page</title></head>')
+    //     res.write('<body><h2>Welcome to About us page</h2></body>')
+    //     res.write('</html>')
+    //     res.end()
+    // }
+    // else if (req.url=='/node'){
+    //     res.write('<html>')
+    //     res.write('<head><title>My First Page</title></head>')
+    //     res.write('<body><h2>Welcome to Node project page</h2></body>')
+    //     res.write('</html>')
+    //     res.end()
+    // }
 
+    if(req.url==='/message' && req.method==='POST'){
+        const body = []
+        req.on('data',(chunk)=>{
+            console.log(chunk)
+            body.push(chunk)
+        })
+        req.on('end',()=>{
+            const parsedBody = Buffer.concat(body).toString()
+            console.log(parsedBody)
+            let message='';
+            parsedBody.split('=')[1].split('+').forEach((ele)=>{
+                message+=(ele+' ')
+                
+            });
+            message=message.trim()
+
+            console.log(message)
+            fs.writeFile('message.txt',message+' ' +count, err=>{
+                count++
+                res.statusCode=302
+                res.setHeader('Location','/')
+                return res.end()
+            })
+        })
+        
+        
+    }
+    // console.log(`reqURL ${req.toArray},resURL ${res}`)
 })
 server.listen(4000)
